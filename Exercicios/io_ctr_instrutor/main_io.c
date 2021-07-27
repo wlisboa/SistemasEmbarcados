df6fe9c0150a85d5e778079feb2398c6c555c300
Washington Pinto Lisboa <wlisboa.hc@gmail.com>
2021-07-26 21:08:40 -0400

    Inclusão de codigo de pinmode e writedigital

--- Exercicios/io_ctr_instrutor/main_io.c
+++ Exercicios/io_ctr_instrutor/main_io.c
@@ -48,12 +48,95 @@ Definições dos pinos analogicos
 #define A4     18
 #define A5     19
 
+typedef struct{
+    uint16_t uiPort;
+    uint16_t uiDdr;
+    uint16_t uiBit;
+}BIT_PORT_REG;
+
+BIT_PORT_REG bprPortMap[20];
+
+BIT_PORT_REG setBIT_PORT_REG(uint16_t uiPort,
+                            uint16_t  uiDdr,
+                            uint16_t uiBit){
+    BIT_PORT_REG bprLoad;
+    bprLoad.uiPort =  uiPort;
+    bprLoad.uiDdr =  uiDdr;
+    bprLoad.uiBit =  uiBit;
+    return bprLoad;
+}
+
+void mapPorts(void){
+    bprPortMap[D0] = setBIT_PORT_REG(&PORTD, &DDRD, PD0);
+    bprPortMap[D1] = setBIT_PORT_REG(&PORTD, &DDRD, PD1);
+    bprPortMap[D2] = setBIT_PORT_REG(&PORTD, &DDRD, PD2);
+    bprPortMap[D3] = setBIT_PORT_REG(&PORTD, &DDRD, PD3);
+    bprPortMap[D4] = setBIT_PORT_REG(&PORTD, &DDRD, PD4);
+    bprPortMap[D5] = setBIT_PORT_REG(&PORTD, &DDRD, PD5);
+    bprPortMap[D6] = setBIT_PORT_REG(&PORTD, &DDRD, PD6);
+    bprPortMap[D7] = setBIT_PORT_REG(&PORTD, &DDRD, PD7);
+    bprPortMap[D8] = setBIT_PORT_REG(&PORTB, &DDRB, PB0);
+    bprPortMap[D9] = setBIT_PORT_REG(&PORTB, &DDRB, PB1);
+    bprPortMap[D10]= setBIT_PORT_REG(&PORTB, &DDRB, PB2);
+    bprPortMap[D11]= setBIT_PORT_REG(&PORTB, &DDRB, PB3);
+    bprPortMap[D12]= setBIT_PORT_REG(&PORTB, &DDRB, PB4);
+    bprPortMap[D13]= setBIT_PORT_REG(&PORTB, &DDRB, PB5);
+    bprPortMap[A0] = setBIT_PORT_REG(&PORTC, &DDRC, PC0);
+    bprPortMap[A1] = setBIT_PORT_REG(&PORTC, &DDRC, PC1);
+    bprPortMap[A2] = setBIT_PORT_REG(&PORTC, &DDRC, PC2);
+    bprPortMap[A3] = setBIT_PORT_REG(&PORTC, &DDRC, PC3);
+    bprPortMap[A4] = setBIT_PORT_REG(&PORTC, &DDRC, PC4);
+    bprPortMap[A5] = setBIT_PORT_REG(&PORTC, &DDRC, PC5);
+}
+
 /***************************************************
 void pinMode(int pin, int dir)
 Descrição: Configura os pinos do chip como entrada
 ou como saída
 ***************************************************/
 void pinMode(int pin, int dir){
+    uint16_t *uiptrDdr;
+    uint16_t uiBit;
+    
+    uiptrDdr = bprPortMap[pin].uiDdr;
+    uiBit = bprPortMap[pin].uiBit;
+
+    if (dir == INPUT){
+        //Configura como entrada
+        *uiptrDdr &= ~(1<<uiBit );
+    }
+    else{
+        //Configura como saída
+        *uiptrDdr &= ~(1<<uiBit );
+    }
+
+}
+
+/***************************************************
+Descrição: 
+***************************************************/
+void digitalWrite(int pin, int value){
+    uint16_t *uiptrPort;
+    uint16_t uiBit;
+    uiptrPort = bprPortMap[pin].uiPort;
+    uiBit = bprPortMap[pin].uiBit;
+
+    if (value == FALSE){
+        //Configura como entrada
+        *uiptrPort &= ~(1<<uiBit);
+    }
+    else{
+        //Configura como saída
+        *uiptrPort |= (1<<uiBit);
+    }
+}
+
+/***************************************************
+void pinMode(int pin, int dir)
+Descrição: Configura os pinos do chip como entrada
+ou como saída
+***************************************************/
+void pinMode_old(int pin, int dir){
     //Configuração do D0
     if (pin == D0){
         if (dir == INPUT){
@@ -216,7 +299,7 @@ void pinMode(int pin, int dir){
             //Configura como entrada
             DDRB = DDRB & 0b11011111;         
         }
-        else{
+        else {
             //Configura como saída
             DDRB = DDRB | 0b00100000;
         }
@@ -300,7 +383,7 @@ void digitalWrite(int pin, int value)
 Descrição: Escreve um valor TRUE ou FALSE 
 em um determinado pino.
 ***************************************************/
-void digitalWrite(int pin, int value){
+void digitalWrite_old(int pin, int value){
     
     //Configuração do D0
     if (pin == D0){
@@ -328,7 +411,8 @@ void digitalWrite(int pin, int value){
 }
 
 int main(){
-   pinMode(D13,OUTPUT);
+    mapPorts();
+    pinMode(D13,OUTPUT);
     while(1) { 
         digitalWrite(D13,TRUE);
         _delay_ms(100);
